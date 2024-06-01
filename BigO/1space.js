d4aff9a1f78866426c489ee59f0f026ed092a6a1
vignesh mvgs <vigneshbalanmvgs2003@gmail.com>
2024-06-01 16:45:46 +0530

    changes

--- BigO/1space.js
+++ BigO/1space.js
@@ -7,7 +7,7 @@ function booo(n) {
 booo([1, 2, 3, 4, 5]); // O(1)
 
 function arrayOfHintTimes(n) {
-  let hiArray = [];
+  const hiArray = [];
   for (let i = 0; i < n; i++) {
     hiArray[i] = "hi";
   }
