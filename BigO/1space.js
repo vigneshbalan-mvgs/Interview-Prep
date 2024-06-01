3dbbd7fa35500c687f460fc273c1afe286530071
vignesh mvgs <vigneshbalanmvgs2003@gmail.com>
2024-06-01 16:43:56 +0530

    space complexity

--- BigO/1space.js
+++ BigO/1space.js
@@ -1,19 +1,18 @@
-function booo(n){
-    for(let i=0; i< n.length; i++){
-        console.log("boooooooo!");
-    }
+function booo(n) {
+  for (let i = 0; i < n.length; i++) {
+    console.log("boooooooo!");
+  }
 }
 
+booo([1, 2, 3, 4, 5]); // O(1)
 
-booo([1,2,3,4,5])// O(1)
-
-
-function arrayOfHintTimes(n){
-    let hiArray = [];
-    for (let i = 0; i < n; i++){
-        hiArray[i] = 'hi';
-    }
-    return hiArray;
+function arrayOfHintTimes(n) {
+  let hiArray = [];
+  for (let i = 0; i < n; i++) {
+    hiArray[i] = "hi";
+  }
+  return hiArray;
 }
 
-console.log(arrayOfHintTimes(6));// O(n)
+console.log(arrayOfHintTimes(6)); // O(n)
+
