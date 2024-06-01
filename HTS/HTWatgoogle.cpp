fae16a97d40ea6430f62bfa7a3bd1a06942eb960
vignesh mvgs <vigneshbalanmvgs2003@gmail.com>
2024-06-01 22:22:10 +0530

    How do solve a problem

---
+++ HTS/HTWatgoogle.cpp
@@ -0,0 +1,12 @@
+// given items are [1,2,4,4] sum = 8  //yes
+// given items are [1,2,3,9] sum = 8 //no
+// it going to pair up with any two number from the array that can run through
+// it and find best answer to it sum of the given number kk
+//  sort and non sort
+// it can be any two number to find it
+//  there in array
+//
+//  there would be same number appear but no same element
+//
+
+#include "studio.h"
