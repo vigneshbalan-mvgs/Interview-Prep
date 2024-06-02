5f8a55b2b0d17285e3f046b48b2f5b530ea56c1c
vignesh mvgs <vigneshbalanmvgs2003@gmail.com>
2024-06-02 11:47:55 +0530

    added Array

---
+++ Array/1Array.js
@@ -0,0 +1,19 @@
+const strings = ["a", "b", "c", "d", "e"];
+//4*4 = 16 bytes  of storage
+
+console.log(strings[2]);
+
+//push();
+strings.push("f"); //O(1)
+
+console.log(strings);
+
+//pop
+strings.pop();
+strings.pop(); //O(1)
+
+//unshift
+strings.unshift("x"); //add at the starting "a" ==> "x"
+//O(n)
+
+console.log(strings);
---
+++ HTS/2pairsum.js
@@ -0,0 +1,31 @@
+//native
+
+function hasPairWithSum(arr, sum) {
+  var len = arr.length;
+  for (var i = 0; i < len - 1; i++) {
+    for (var j = i + 1; j < len; j++) {
+      if (arr[i] + arr[j] === sum) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+hasPairWithSum([6, 2, 3, 5, 1, 2, 5, 1, 2, 5, 8, 4], 12);
+
+//better version
+
+function hasPairWithSum2(arr, sum) {
+  const mySet = new Set();
+  const len = arr.length;
+  for (let i = 0; i < len; i++) {
+    if (mySet.has(arr[i])) {
+      return true;
+    }
+    mySet.add(sum - arr[i]);
+  }
+  return false;
+}
+
+hasPairWithSum2([6, 2, 3, 5, 1, 2, 5, 1, 2, 5, 8, 4], 12);
