816f1ce161af09a0fd3fbcf684a549b22fafe50d
vignesh mvgs <vigneshbalanmvgs2003@gmail.com>
2024-06-03 17:29:03 +0530

    Hashtable.js

---
+++ HashTable/1Hashtable.js
@@ -0,0 +1,13 @@
+let user = {
+  age: 54,
+  name: "kylie",
+  magic: true,
+  screen: function () {
+    console.log("ahhhhh");
+  },
+};
+
+console.log(user.age);
+console.log(user.spell = "abra kadabra");
+console.log(user);
+user.screen();
