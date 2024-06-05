9174be6a459c60b931ab377de1b2d66e09c182f6
vignesh mvgs <vigneshbalanmvgs2003@gmail.com>
2024-06-05 10:24:24 +0530

    2Hashtable.js

--- HashTable/2Hashtable.js
+++ HashTable/2Hashtable.js
@@ -6,7 +6,7 @@ class HashTable {
     let hash = 0; // create a new varial that is 0
     for (let i = 0; i < key.length; i++) { // key is grep the length of input
       hash = (hash + key.charCodeAt(i) * 1) % this.data.length;// charCodeAt(); => UTF-16
-      
+
     }
     return hash;
   }
@@ -16,11 +16,47 @@ class HashTable {
     if (!this.data[address]) {
       this.data[address] = [];
       this.data[address].push([key, value]);
-      console.log(this.data);
     }
+  }
+
+  get(key) {
+    let address = this._hash(key);
+    const currentBucket = this.data[address];
+    console.log(currentBucket);
+    if (currentBucket) {
+      for (let i = 0; i < currentBucket.length; i++) {
+        if (currentBucket[i][0] === key) {
+          return currentBucket[i][1]
+        }
+      }
+    }//O(n)
+    return undefined
+  }
+  keys() {
+    if (!this.data.length) {
+      return undefined
+    }
+    let result = []
+    // loop through all the elements
+    for (let i = 0; i < this.data.length; i++) {
+      // if it's not an empty memory cell
+      if (this.data[i] && this.data[i].length) {
+        // but also loop through all the potential collisions
+        if (this.data.length > 1) {
+          for (let j = 0; j < this.data[i].length; j++) {
+            result.push(this.data[i][j][0])
+          }
+        } else {
+          result.push(this.data[i][0])
+        }
+      }
+    }
+    return result;
   }
 }
 
-const myHashTable = new HashTable(2);
+const myHashTable = new HashTable(50);
 myHashTable.set("grapes", 10000);
-myHashTable._hash("grapes", 10000);
+myHashTable.set("apples", 54);
+myHashTable.set("orange", 2);
+myHashTable.keys();
